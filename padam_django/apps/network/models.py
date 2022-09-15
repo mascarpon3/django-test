9a53284b348eb921ba2df6c518b937c91eaed931
charles <charles.englebert@protonmail.com>
2022-09-15 18:03:39 +0200

    improve models

--- padam_django/apps/network/models.py
+++ padam_django/apps/network/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 from padam_django.apps.geography.models import Place
+from padam_django.apps.users.models import User
+from padam_django.apps.fleet.models import Bus
 
 
-# Create your models here.
 class BusStop(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
 
+    def save(self, *args, **kwargs):
+        BusStop.objects.all()
+        super(BusStop, self).save(*args, **kwargs)
+
 
 class BusShift(models.Model):
-    bus_stops = models.OneToOneField(BusStop, on_delete=models.CASCADE)
+    start_datatime = models.DateTimeField(null=False)
+    end_datatime = models.DateTimeField(null=False)
+    driver = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    bus = models.ForeignKey(Bus, null=False, on_delete=models.CASCADE)
+
+
+class BusShiftToBusStops(models.Model):
+    bus_shift = models.ForeignKey(BusShift, on_delete=models.CASCADE)
+    bus_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
+    position = models.IntegerField(null=False)
