acf57ba783deccf405bd9236048a8d647e6d2a9b
charles <charles.englebert@protonmail.com>
2022-09-16 10:53:53 +0200

    improve models

--- padam_django/apps/network/models.py
+++ padam_django/apps/network/models.py
@@ -1,25 +1,14 @@
 from django.db import models
 from padam_django.apps.geography.models import Place
-from padam_django.apps.users.models import User
-from padam_django.apps.fleet.models import Bus
+from padam_django.apps.fleet.models import Bus, Driver
 
 
 class BusStop(models.Model):
     place = models.ForeignKey(Place, on_delete=models.CASCADE)
-
-    def save(self, *args, **kwargs):
-        BusStop.objects.all()
-        super(BusStop, self).save(*args, **kwargs)
+    pick_up_time = models.TimeField(null=False)
 
 
 class BusShift(models.Model):
-    start_datatime = models.DateTimeField(null=False)
-    end_datatime = models.DateTimeField(null=False)
-    driver = models.ForeignKey(User, null=False, on_delete=models.CASCADE)
+    driver = models.ForeignKey(Driver, null=False, on_delete=models.CASCADE)
     bus = models.ForeignKey(Bus, null=False, on_delete=models.CASCADE)
-
-
-class BusShiftToBusStops(models.Model):
-    bus_shift = models.ForeignKey(BusShift, on_delete=models.CASCADE)
-    bus_stop = models.ForeignKey(BusStop, on_delete=models.CASCADE)
-    position = models.IntegerField(null=False)
+    bus_stops = models.ManyToManyField(BusStop)
--- padam_django/apps/network/tests.py
+++ padam_django/apps/network/tests.py
@@ -1,19 +1,54 @@
+import datetime
+from random import randrange, seed
+
 from django.test import TestCase
-from padam_django.apps.network.models import BusStop, BusShift
+from django.core import management
 
 from padam_django.apps.geography.models import Place
-from django.core import management
+from padam_django.apps.network.models import BusStop, BusShift
+from padam_django.apps.fleet.models import Bus, Driver
 
 
 class NetworkTestCse(TestCase):
     def setUp(self):
+        seed(1)
         management.call_command('create_users', number=5)
         management.call_command('create_drivers', number=5)
         management.call_command('create_buses', number=10)
         management.call_command('create_places', number=30)
+        create_stops()
+        create_bus_shift()
 
     def test_add_bus_shift(self):
-        place = Place.objects.first()
-        bus_stop = BusStop(place=place)
-        bus_stop.save()
         assert False
+
+
+def create_stops():
+    for place in Place.objects.all():
+        BusStop(place=place, pick_up_time=_random_time()).save()
+
+
+def create_bus_shift():
+    bus_stops = BusStop.objects.all()
+    buss = Bus.objects.all()
+    drivers = Driver.objects.all()
+
+    bus_shift = BusShift.objects.create(bus=buss[0], driver=drivers[0])
+    bus_shift.bus_stops.add(bus_stops[0], bus_stops[1], bus_stops[2])
+    bus_shift.save()
+
+    bus_shift = BusShift.objects.create(bus=buss[1], driver=drivers[1])
+    bus_shift.bus_stops.add(bus_stops[3], bus_stops[4], bus_stops[5])
+    bus_shift.save()
+
+    bus_shift = BusShift.objects.create(bus=buss[2], driver=drivers[2])
+    bus_shift.bus_stops.add(bus_stops[6], bus_stops[7], bus_stops[8])
+    bus_shift.save()
+
+    bus_shift = BusShift.objects.create(bus=buss[3], driver=drivers[3])
+    bus_shift.bus_stops.add(bus_stops[6], bus_stops[7], bus_stops[8], bus_stops[9])
+    bus_shift.save()
+
+
+def _random_time():
+    return datetime.time(randrange(0, 24), randrange(0, 60),)
